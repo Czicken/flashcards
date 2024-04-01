9d79da2c0f3eba30c1a7a09ca3ac6d4a42ff5476
valivia <26413018+valivia@users.noreply.github.com>
2024-04-01 12:43:51 +0000

    innit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import cards from "./cards.json";
+
+    let counter = 0;
+    function onClick() {}
+</script>
+
+<div>
+    <h1>{cards[0].answer}</h1>
+    {counter}
+    <button on:click={onClick}>Click me</button>
+</div>
+
+<style lang="scss">
+    h1 {
+        color: red;
+    }
+</style>
